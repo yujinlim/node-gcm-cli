596822d9b4687e09c0752a0f22ab434de5068a8c
yujinlim <adamlyj89@gmail.com>
2014-12-11 17:11:52 +0800

    update cli options helper

--- cli.js
+++ cli.js
@@ -12,7 +12,7 @@ program
   .version(version)
   .option('-m, --message <message>', 'message that you wish to send')
   .option('-k, --key <key>', 'google api key')
-  .option('-r, --registrationids <ids>', 'registrationids, if more than one', list);
+  .option('-r, --registrationids <ids>', 'comma separated ids, if more than one', list);
 
 program.parse(process.argv);
 
