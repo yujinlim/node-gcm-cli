6785e8ccc2679bb645fb02c543abb967397e5e49
yujinlim <adamlyj89@gmail.com>
2014-12-14 23:11:04 +0800

    add other gcm flags

--- cli.js
+++ cli.js
@@ -21,19 +21,20 @@ function toObject(val) {
 program
   .version(version)
   .option('-m, --message <message>', 'message that you wish to send (string or key-value), eg: --message="message:this is test,path:landing"')
-  .option('-k, --key <key>', 'google api key')
+  .option('-k, --apiKey <key>', 'google api key')
   .option('-r, --registrationids <ids>', 'comma separated ids, if more than one', list)
-  .option('-j, --json', 'message is json string (boolean), default to false', false);
+  .option('-j, --json', 'message is json string (default to false)', false)
+  .option('-c, --collapseKey <key>', 'collapse key for android')
+  .option('-d, --delayWhileIdle', '`If the device is connected but idle, the message will still be delivered right away unless the delayWhileIdle flag is set to true`, (default to false)', false)
+  .option('-t, --timeToLive <time>', 'alive time of this notification')
+  .option('-D, --dryRun', 'for debug/ test purpose, it allows for sending a message without really sending it (default to false)', false);
 
 program.parse(process.argv);
 
-if (program.message && program.key && program.registrationids) {
+if (program.message && program.apiKey && program.registrationids) {
   if (program.json)
     program.message = toObject(program.message);
-
-  gcm(program.message, program.registrationids, {
-    apiKey: program.key
-  }, function(err, response){
+  gcm(program.message, program.registrationids, program, function(err, response){
     console.log(err, response);
   });
 }
